0bf74138e0752221b0fb03e6da7086d3a9ad2a8c
Arkadiusz Waluk <arkadiusz@waluk.pl>
2021-01-22 19:04:35 +0100

    Update project - change list() to [] and small refactor

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once 'functions.php';
 
 $data = array_fill(0, 3, null);
 $argv = array_replace($data, $argv);
-list($filename, $command, $content) = $argv;
+[$filename, $command, $content] = $argv;
 
 switch ($command) {
     case 'add':
@@ -25,9 +25,9 @@ switch ($command) {
     case null:
         $tasks = readFromFile();
         foreach ($tasks as $number => $task) {
-            echo ($number + 1).' | '.$task.PHP_EOL;
+            echo ($number + 1) . ' | ' . $task . PHP_EOL;
         }
-        echo '-----'.PHP_EOL.'All tasks: '.count($tasks);
+        echo '-----' . PHP_EOL . 'All tasks: ' . count($tasks);
         break;
 
     default:
@@ -35,3 +35,4 @@ switch ($command) {
         break;
 }
 echo PHP_EOL;
+
